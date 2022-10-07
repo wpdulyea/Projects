93dc6b2dd0b62d82a2596ba6a19194e045c0bd94
William Dulyea <wpdulyea@yahoo.com>
2022-10-07 12:47:21 +0100

    Missing source files added

---
+++ microbit/cpp/source/main.cpp
@@ -0,0 +1,48 @@
+#include "MicroBit.h"
+
+MicroBit    uBit;
+
+// Let's set up some event listeners to let hardware interrupts handle context swwitching
+
+void onButton(MicroBitEvent e)
+{
+	if (e.source == MICROBIT_ID_BUTTON_A) {
+        	uBit.serial.printf("BUTTON A: ");
+    		uBit.radio.datagram.send("A");
+    	}
+	else if (e.source == MICROBIT_ID_BUTTON_B) {
+        	uBit.serial.printf("BUTTON B: ");
+		uBit.radio.datagram.send("B");
+	}
+    	uBit.serial.printf("\r\n");
+}
+
+void onData(MicroBitEvent)
+{
+	ManagedString str = uBit.radio.datagram.recv();
+	uBit.serial.printf("RX DATA");
+	uBit.display.scroll(str);
+}
+
+int main()
+{
+	// Initialise the micro:bit runtime.
+    	uBit.init();
+	// Register to receive events.
+	uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButton);
+	uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButton);
+    	uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
+    	if( uBit.radio.enable() != MICROBIT_OK) {
+		uBit.display.scroll("Failed to enable the Radio", 100);
+	}
+	uBit.radio.setGroup(10);
+	uBit.radio.setTransmitPower(6);
+	uBit.radio.setFrequencyBand(10);
+
+
+    	while(1) {
+		uBit.serial.printf("Sleep cycle in main.\r\n");
+		uBit.sleep(1000);
+	}
+}
+
