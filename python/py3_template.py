371476ec84f1ab16ffcd6b893f54424af472ac9f
William Dulyea <wpdulyea@yahoo.com>
2022-11-11 14:59:13 +0000

    updated template

--- python/py3_template.py
+++ python/py3_template.py
@@ -1,37 +1,48 @@
 #!/usr/bin/env python3
-__author__ = "William Dulyea"
+"""
+Description:
+"""
+# -----------------------------------------------------------------------------
+#                               Safe Imports
+# -----------------------------------------------------------------------------
+# Standard
+import sys
+import os
+import re
+from time import sleep
+from traceback import format_exc
+from random import choice, randint, uniform
+import ssl
+import json
+from datetime import datetime
+from threading import Thread
+from multiprocessing import Process
+import requests
+from netaddr import IPNetwork
+
+# Third party
+from nslookup import Nslookup
+
+# local
+
+
+# -----------------------------------------------------------------------------
+#                           Global definitions
+# -----------------------------------------------------------------------------
+__author__ = "Copyright (c) 2022, W P Dulyea, All rights reserved."
 __email__ = "wpdulyea@yahoo.com"
-
-try:
-    import sys
-    import os
-    from time import sleep
-    from traceback import format_exc
-    from random import choice
-    import ssl
-    from random import randint, uniform
-    import json
-    from datetime import datetime
-    from threading import Thread
-    from multiprocessing import Process
-    from nslookup import Nslookup
-    from argparse import ArgumentParser
-    from copy import deepcopy
-    from netaddr import IPNetwork
-    import re
-    import requests
-
-except ImportError as err:
-    print(f"Module import failed due to {err}")
-    sys.exit(1)
-
+__version__ = "$Name: Release 0.1.0 $"[7:-2]
 
 
 def read_inputs():
+    from argparse import ArgumentParser
+
     res = None
     try:
         parser = ArgumentParser()
-        parser.add_argument("-a", help="Is for Apple", type=str, required=false)
+        parser.add_argument(
+            "-a", help="Is for Apple", type=str, required=False
+        )
         parser.add_argument(
             "-b", help="Is for how many Ballon(s)", type=int, required=True
         )
@@ -42,6 +53,9 @@ def read_inputs():
         return res
 
 
+# -----------------------------------------------------------------------------
+#                               If run as main file
+# -----------------------------------------------------------------------------
 if __name__ == "__main__":
 
     cmd_name = os.path.splitext(os.path.basename(__file__))[0]
@@ -51,17 +65,12 @@ if __name__ == "__main__":
         sys.exit(1)
 
     try:
-        ns = Nslookup()
-        if args.a:
-            print(f'Got agrs.a {args.a}')
-
-        if args.b:
-            print(f'Got args.b {args.b} is pseciofied')
+        ...
 
     except KeyboardInterrupt:
-        logger.error("Aborted the script execution through keyboard interrupt")
-        sys.exit()
+        print("Aborted the script execution through keyboard interrupt")
+        sys.exit(0)
     except Exception as err:
-        logger.error(str(err))
+        print(str(err))
         print(format_exc())
-        sys.exit()
+        sys.exit(1)
